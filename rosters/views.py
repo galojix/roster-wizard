ae12248d3c3ba8365bd247d90e525068e61e2dfa
Greg Cowell <greg.cowell@gmail.com>
2019-07-01 16:33:51 +1000

    Reduce solver timeout again

--- rosters/views.py
+++ rosters/views.py
@@ -736,7 +736,7 @@ class GenerateRosterView(LoginRequiredMixin, FormView):
 
         # Create the solver and solve
         solver = cp_model.CpSolver()
-        solver.parameters.max_time_in_seconds = 100
+        solver.parameters.max_time_in_seconds = 90
         solution_status = solver.Solve(model)
         if (
             solution_status != cp_model.FEASIBLE
