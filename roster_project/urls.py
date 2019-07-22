401388243eea81a4eadea24792f18b6fd11ea3fb
Greg Cowell <greg.cowell@gmail.com>
2019-07-22 12:30:34 +1000

    Change admin URL

--- roster_project/urls.py
+++ roster_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("dbmanage/", admin.site.urls),
     # path('users/', include('users.urls')),
     path("users/", include("django.contrib.auth.urls")),
     path("rosters/", include("rosters.urls")),
