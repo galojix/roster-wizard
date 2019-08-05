37baeb21009ab8362aba603b0832a4a6fd498b5f
Greg Cowell <greg.cowell@gmail.com>
2019-08-05 16:21:06 +1000

    Update test

--- rosters/tests/test_models.py
+++ rosters/tests/test_models.py
@@ -6,15 +6,16 @@ from ..models import Day
 
 class DayTests(TestCase):
     def setUp(self):
-        Day.objects.create(number=5)
+        self.day = Day.objects.create(number=5)
 
     def test_day_content(self):
-        day = Day.objects.get(id=1)
-        expected_object_number = 5
-        self.assertEquals(expected_object_number, 5)
+        self.assertEquals(self.day.number, 5)
 
     def test_day_detail_view(self):
-        response = self.client.get(reverse("day_detail", kwargs={"pk": 1}))
+        response = self.client.get(
+            reverse("day_detail", kwargs={"pk": self.day.id})
+        )
+        print("Location: ", response['location'])
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 5)
         self.assertTemplateUsed(response, "day_detail.html")
