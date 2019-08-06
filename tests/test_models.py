178972ced00a94a382148bc056d9ce3c6be53fa2
Greg Cowell <greg.cowell@gmail.com>
2019-08-06 14:03:05 +1000

    Add another test

--- tests/test_models.py
+++ tests/test_models.py
@@ -25,7 +25,7 @@ def test_user(init_db):
 
 
 @pytest.mark.django_db
-def test_day(init_db, client):
+def test_day_detail_view(init_db, client):
     day = Day.objects.create(number=5)
     client.login(username='temporary', password='temporary')
     response = client.get(
@@ -34,3 +34,12 @@ def test_day(init_db, client):
     assert response.status_code == 200
     assert "Day: 5" in response.rendered_content
     assert 'day_detail.html' in [t.name for t in response.templates]
+
+
+@pytest.mark.django_db
+def test_roster_list_view(init_db, client):
+    client.login(username='temporary', password='temporary')
+    response = client.get(reverse("roster_list"))
+    assert response.status_code == 200
+    assert "Roster:" in response.rendered_content
+    assert 'roster_list.html' in [t.name for t in response.templates]
