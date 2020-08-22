142fe9a739fb63ecb8857ccea66569b9eead137c
Greg Cowell <greg.cowell@gmail.com>
2020-08-22 12:49:42 +1000

    Cleanup user views module

--- users/views.py
+++ users/views.py
@@ -1,29 +1,36 @@
+"""Views."""
+
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
-
+# from django.core.exceptions import PermissionDenied
 from .models import CustomUser
-from rosters.models import Role
 
 
 class UserListView(LoginRequiredMixin, ListView):
+    """UserListView."""
+
     model = CustomUser
     template_name = "user_list.html"
     login_url = "login"
 
     def get_queryset(self):
+        """Get query set."""
         return CustomUser.objects.order_by("last_name", "first_name")
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
+    """UserDetailView."""
+
     model = CustomUser
     template_name = "user_detail.html"
     login_url = "login"
 
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
+    """UserUpdateView."""
+
     model = CustomUser
     template_name = "user_edit.html"
     fields = (
@@ -40,6 +47,8 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class UserDeleteView(LoginRequiredMixin, DeleteView):
+    """UserDeleteView."""
+
     model = CustomUser
     template_name = "user_delete.html"
     success_url = reverse_lazy("user_list")
@@ -47,6 +56,8 @@ class UserDeleteView(LoginRequiredMixin, DeleteView):
 
 
 class UserCreateView(LoginRequiredMixin, CreateView):
+    """UserCreateView."""
+
     model = CustomUser
     template_name = "user_new.html"
     fields = (
