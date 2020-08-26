996d1b9a333a16ee44fc85941c632f8382fb1765
Greg Cowell <greg.cowell@gmail.com>
2020-08-27 08:03:02 +1000

    Tidy up URLs

--- roster_project/urls.py
+++ roster_project/urls.py
@@ -6,9 +6,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("dbmanage/", admin.site.urls),
-    # path('users/', include('users.urls')),
-    path("users/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("rosters/", include("rosters.urls")),
-    path("customusers/", include("users.urls")),
+    path("users/", include("users.urls")),
     path("", include("pages.urls")),
 ]
