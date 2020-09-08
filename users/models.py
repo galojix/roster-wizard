84a01f1fdae6f551b4f78fc6cdd8afe9c52423af
Greg Cowell <greg.cowell@gmail.com>
2020-09-09 09:48:24 +1000

    Allow blank roles when adding staff

--- users/models.py
+++ users/models.py
@@ -25,7 +25,7 @@ class CustomUser(AbstractUser):
     available = models.BooleanField(null=False, blank=False, default=True)
     shifts_per_roster = models.IntegerField(null=False, blank=False, default=0)
     max_shifts = models.BooleanField(null=False, blank=False, default=True)
-    roles = models.ManyToManyField("rosters.Role")
+    roles = models.ManyToManyField("rosters.Role", blank=True)
 
     def __str__(self):
         """How a user object is displayed."""
