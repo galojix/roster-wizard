dbb2c769d1a8bedc8dd002220d75a1e5b5d5a72f
Greg Cowell <greg.cowell@gmail.com>
2020-11-03 10:31:51 +1000

    Change leaves to leave

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from rest_framework.routers import SimpleRouter
 from .views import LeaveViewSet, TimeSlotViewSet
 
 router = SimpleRouter()
-router.register("leaves", LeaveViewSet, basename="leaves")
+router.register("leave", LeaveViewSet, basename="leave")
 router.register("timeslots", TimeSlotViewSet, basename="timeslots")
 urlpatterns = router.urls
