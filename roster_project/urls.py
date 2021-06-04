97db96a3ddd2c7b54c74fe4fddec5d93f80f5302
Greg Cowell <greg.cowell@gmail.com>
2021-06-04 14:11:20 +1000

    Add site style to password change

--- roster_project/urls.py
+++ roster_project/urls.py
@@ -3,11 +3,28 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+from django.contrib.auth.views import (
+    PasswordChangeDoneView,
+    PasswordChangeView,
+)
 
 urlpatterns = [
     path("dbmanage/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path(
+        "password_change/",
+        PasswordChangeView.as_view(
+            template_name="registration/password_change.html"
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change_done/",
+        PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
     path("rosters/", include("rosters.urls")),
     path("api/v1/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
