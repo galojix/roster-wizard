d38965dc199c511b397cbea806aed002c85c5e18
Greg Cowell <greg.cowell@gmail.com>
2023-06-07 12:54:29 +1000

    Update packages

--- rosters/tasks.py
+++ rosters/tasks.py
@@ -1,4 +1,5 @@
 """Celery tasks."""
+from datetime import datetime
 from dateutil import parser
 
 from celery import shared_task
@@ -8,7 +9,8 @@ from .logic import RosterGenerator
 @shared_task()
 def generate_roster(start_date):
     """Generate roster."""
-    start_date = parser.isoparse(start_date)
+    if not isinstance(start_date, datetime):
+        start_date = parser.isoparse(start_date)
     roster = RosterGenerator(start_date=start_date)
     roster.create()
     return "Roster is complete..."
