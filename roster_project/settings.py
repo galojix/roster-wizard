88442853fc3387b5a4fa1494f43290b37d9ba2b8
Greg Cowell <greg.cowell@gmail.com>
2023-09-25 15:42:08 +1000

    Update packages

--- roster_project/settings.py
+++ roster_project/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 if DEBUG and TOOLBAR:
