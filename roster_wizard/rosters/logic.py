07f3acdea8aaef4b403086241fdfc7e72b2f4d46
Greg Cowell <greg.cowell@gmail.com>
2025-07-21 10:09:22 +1000

    Rename staffruleshifts to shiftsequenceshifts

--- roster_wizard/rosters/logic.py
+++ roster_wizard/rosters/logic.py
@@ -175,10 +175,10 @@ class RosterGenerator:
         Example: { 1: [ "E", "L", "N"], 2: ["E", "L"] }
         """
         shift_sequence = OrderedDict()
-        staffruleshifts = shiftsequence.shiftsequenceshift_set.order_by("position")
-        for staffruleshift in staffruleshifts:
-            shift_sequence.setdefault(staffruleshift.position, []).append(
-                staffruleshift.shift
+        shiftsequenceshifts = shiftsequence.shiftsequenceshift_set.order_by("position")
+        for shiftsequenceshift in shiftsequenceshifts:
+            shift_sequence.setdefault(shiftsequenceshift.position, []).append(
+                shiftsequenceshift.shift
             )
         return shift_sequence
 
